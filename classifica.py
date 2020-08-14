cc7a0c9b1a1131c0995801e937dadd5430d5815c
Danilo Dominoni <danilo@MacBook-Pro-de-Danilo.local>
2020-08-14 20:39:23 -0300

    Initial commit - :D

---
+++ classifica.py
@@ -0,0 +1,34 @@
+import pandas as pd
+df = pd.read_csv('data.csv')
+X_df = df[['home', 'busca', 'logado']]
+Y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(X_df).astype(int)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+tamanho_treino = int(0.9 * len(Y))
+tamanho_teste = len(Y) - tamanho_treino
+
+treino_dados = X[:tamanho_treino]
+treino_marcacoes = Y[:tamanho_treino]
+
+teste_dados = X[-tamanho_teste:]
+teste_marcacoes = Y[-tamanho_teste:]
+
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+total_acertos = len(acertos)
+total_elementos = len(teste_marcacoes)
+taxa_acertos = 100.0 * total_acertos / total_elementos
+
+print(taxa_acertos)
+print(total_elementos)
